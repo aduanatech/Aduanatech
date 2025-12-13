f4a1e21301871d41a7bf0b5af7b7d5e68bd4cf10
SiroCarv <joaecitocarvajal@gmail.com>
2025-12-12 22:09:24 -0400

    Delete proyecto analisis y diseño directory

--- proyecto analisis y diseño/inicio/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-# Create your views here.
-def hol(request):
-    return HttpResponse("<h2>Eso Tilin<h2>")
-def hol2(resquest):
-    return HttpResponse('About')
--- proyecto analisis y diseño/usuarios/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import RegistroUsuarioView
-
-urlpatterns = [
-    path('registro/', RegistroUsuarioView.as_view(), name='registro'),
-]
